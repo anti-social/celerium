0b90a2d6b8a6f8a688c4988537d740847475714e
Alexander Koval <kovalidis@gmail.com>
2013-03-05 17:59:52 +0200

    Display workers on the dashboard

--- celerium/searcher.py
+++ celerium/searcher.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 from solar import CommonSearcher
+from solar.result import Document
 
+from celery.events.state import Worker as _Worker, Task
 from celery.utils.timeutils import maybe_iso8601
 
 
@@ -9,8 +11,29 @@ def try_fromtimestamp(timestamp):
     if timestamp:
        return datetime.fromtimestamp(timestamp)
 
+class Worker(_Worker):
+    # increase expire_window cause 200 is too small
+    # Solr doesnt have time to do commit
+    # 400 means freq * 4
+    # if freq == 5 then
+    # worker evaluates as alive during 20 seconds after last heartbeat
+    expire_window = 400
+    
+class WorkerDocument(Document):
+    def __init__(self, raw_doc, _results=None):
+        super(WorkerDocument, self).__init__(raw_doc, _results=_results)
+
+    @property
+    def instance(self):
+        if not hasattr(self, '_instance'):
+            self._instance = Worker(**self.to_solr())
+            for timestamp in self.heartbeats:
+                self._instance.on_heartbeat(timestamp=timestamp)
+        return self._instance
+    
 class WorkerSearcher(CommonSearcher):
     type_value = 'Worker'
+    document_cls = WorkerDocument
     
     @classmethod
     def generate_indexing_doc(cls, worker, project):
@@ -25,9 +48,12 @@ class WorkerSearcher(CommonSearcher):
 
 class TaskSearcher(CommonSearcher):
     type_value = 'Task'
-    
-    datetime_fields = (
-        'timestamp', 'sent', 'received', 'started', 'succeeded', 'expires')
+
+    iso8601_fields = ('eta', 'expires')
+    timestamp_fields = (
+        'timestamp', 'sent', 'received', 'started', 'succeeded',
+        'failed', 'retried', 'revoked', 'expires',
+        )
     
     @classmethod
     def generate_indexing_doc(cls, task, project):
@@ -36,9 +62,10 @@ class TaskSearcher(CommonSearcher):
         doc['project'] = project
         if doc['name'].find('.') != -1:
             doc['module'] = '.'.join(doc['name'].split('.')[:-1])
-        for field in cls.datetime_fields:
+        for field in cls.timestamp_fields:
             doc[field] = try_fromtimestamp(doc[field])
-        doc["eta"] = maybe_iso8601(doc["eta"])
+        for field in cls.iso8601_fields:
+            doc[field] = maybe_iso8601(doc[field])
         if doc['worker']:
             doc['worker'] = doc['worker'].hostname
         return doc
